b4247c68e6002075a1b4adf8a88293df23d9b5b0
ireone01 <huyvip10a3@gmail.com>
2024-07-05 15:46:55 +0700

    cap nhat them chon do kho

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.movieapp
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -53,14 +57,71 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.mi_refresh -> {
-                setupBoard()
+                if(memoryGame.getNummoves()>0 &&  !memoryGame.haveWonGame()){
+                    showAlertDialog("Quit your game ?",null ,View.OnClickListener {
+                        setupBoard()
+                    })
+                }else{
+                    setupBoard()
+                }
+            }
+            R.id.mi_new_size ->{
+                showNewSizeDilog()
+                return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    private fun showNewSizeDilog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size , null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+        when(boardSize){
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose a new size", boardSizeView , View.OnClickListener {
+            boardSize = when(radioGroupSize.checkedRadioButtonId){
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+
+            }
+            setupBoard()
+        })
+    }
+
+    private fun showAlertDialog(title : String, view: View?,positiveClickListener: View.OnClickListener) {
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Ok"){
+                _,_ ->
+                positiveClickListener.onClick(null)
+            }.show()
+
+
+    }
+
 
     private fun setupBoard() {
+        when(boardSize){
+            BoardSize.EASY ->{
+                textView2.text = "EASY 4X2"
+                textView1.text = "Pair: 0/4"
+            }
+            BoardSize.MEDIUM ->{
+                textView2.text = "MEDIUM 6X3"
+                textView1.text = "Pair: 0/9"
+            }
+            BoardSize.HARD -> {
+                textView2.text = "Hard 6X6"
+                textView1.text = "Pair: 0/12"
+            }
+        }
         textView1.setTextColor(ContextCompat.getColor(this,R.color.color_progress_none))
         memoryGame = MemoryGame(boardSize)
         adapter = MemoryBoardAdapter(this,boardSize,memoryGame.cards,object :MemoryBoardAdapter.CardClickListener{
